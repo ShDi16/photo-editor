d564c8a08a6dcf88e1bb244446e005f5c4925e12
ShDi16 <dmitriyshvidko@gmail.com>
2026-08-11 12:49:47 +0300

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,1 +1,18 @@
-console.log('Ads ready');
+window.addEventListener('load', () => {
+  (function(w, d, n, s, t) {
+    w[n] = w[n] || [];
+    w[n].push(function() {
+      Ya.Context.AdvManager.render({
+        blockId: 'R-A-XXXXXX-1',   // ← сюда вставь свой ID блока
+        renderTo: 'ad-slot-bottom',
+        async: true
+      });
+    });
+    t = d.getElementsByTagName('script')[0];
+    s = d.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'https://an.yandex.ru/system/context.js';
+    s.async = true;
+    t.parentNode.insertBefore(s, t);
+  })(this, this.document, 'yandexContextAsyncCallbacks');
+});
